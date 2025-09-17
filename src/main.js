b27910fc4f9955f20f05f824b0673fe5e707cd48
M4noel <manoelmurilo9@gmail.com>
2025-09-17 16:24:46 -0300

    Analytics

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './style.css'
+import { inject } from '@vercel/analytics'
+import { injectSpeedInsights } from '@vercel/speed-insights'
+
+// Inicializar Vercel Analytics e Speed Insights
+inject()
+injectSpeedInsights()
 
 createApp(App).mount('#app')
